4f68ba4e599a95b74a1ddb7f5b0f2e26ea737aad
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-28 12:53:23 +0100

    Create todo model

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import InputField  from './components/InputField'
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
 
-  console.log(todo)
-
   return (
     <div className="App">
       <span className="heading">Taskify</span>
