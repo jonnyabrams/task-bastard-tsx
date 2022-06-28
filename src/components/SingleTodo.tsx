a5feba878d4626e2eefb8f85eb621416766ad2b6
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-28 17:03:57 +0100

    isDone functionality added - strikes through text

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -2,6 +2,7 @@ import { Todo } from '../model'
 import './styles.css'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
+import TodoList from './TodoList'
 
 type Props = {
   todo: Todo,
@@ -10,14 +11,24 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos}: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone} : todo)
+  )}
+
   return (
     <form className="todos_single">
-      <span className="todos_single--text">{todo.todo}</span>
+      {
+        todo.isDone ? (
+          <s className="todos_single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos_single--text">{todo.todo}</span>
+        )
+      }
 
       <div>
         <span className="icon"><AiFillEdit /></span>
         <span className="icon"><AiFillDelete /></span>
-        <span className="icon"><MdDone /></span>
+        <span className="icon" onClick={() => handleDone(todo.id)}><MdDone /></span>
       </div>
     </form>
   )
