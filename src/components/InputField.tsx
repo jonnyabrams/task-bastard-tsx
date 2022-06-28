5f1ebdfe4bc8e0841557d84e370740461b2085cb
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-28 12:47:22 +0100

    Set types of todo and setTodo (hover on latter to find it) + pass them down as props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,11 @@
 import './input_field.css'
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
       <input type="input" placeholder="Enter a task" className="input_box" />
