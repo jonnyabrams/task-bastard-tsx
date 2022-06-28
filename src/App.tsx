4c87f11eeee358436804ecee66fb9f8013deed87
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-28 12:57:29 +0100

    Add handleAdd function to InputField props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([])
 
+  const handleAdd = () => {
+    
+  }
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,11 +3,12 @@ import './input_field.css'
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: () => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a task" className="input_box" />
       <button className="input_submit" type="submit">Go</button>
     </form>
