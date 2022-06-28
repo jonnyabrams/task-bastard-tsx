d2f705ce34bc1005d02defdec40903912076b119
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-28 12:41:17 +0100

    Create InputField with styles

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import './input_field.css'
+
+const InputField = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input_box" />
+      <button className="input_submit" type="submit">Go</button>
+    </form>
+  )
+}
+
+export default InputField
